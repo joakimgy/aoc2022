687f4db02079188bd096b957191d567fa68f8aae
Joakim Gyllenskepp <joakim.gyllenskepp@vegvesen.no>
2022-12-03 09:03:25 +0100

    Add support for reading files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_file("src/input.txt");
+}
+
+fn read_file(path: &str) -> String {
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+    println!("With input:\n{contents}");
+    return contents;
 }
